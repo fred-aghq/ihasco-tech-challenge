84ee188bfb9d45ade25d968766bb727db876a23d
Fred Spooner <fred@aghq.uk>
2023-03-25 16:52:18 +0000

    feat: refactor command

--- app/Console/Commands/QueryUrl.php
+++ app/Console/Commands/QueryUrl.php
@@ -2,75 +2,68 @@
 
 namespace App\Console\Commands;
 
+use App\Services\Proxy\ProxyService;
+use App\Services\UrlQuery\UrlQueryService;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class QueryUrl extends Command
 {
-    protected $signature = 'query:url {url}';
+    protected $signature = 'query:url {url?} --retries=5';
 
     protected $description = 'N/A';
 
-    public function __construct()
-    {
+    public function __construct(
+        private ProxyService $proxyService,
+        private UrlQueryService $urlQueryService
+    ) {
         parent::__construct();
+    }
+
+    private function getProxyList(): array
+    {
+        return $this->proxyService->getProxyList();
+    }
+
+    private function writeHeaders(array $headers): void {
+        foreach($headers as $name => $values) {
+            $this->line($name . ': ' . implode(', ', $values));
+        }
+    }
+
+    private function logRequest(string $url) {
+        $now = Carbon::now()->format('Y-m-d H:i:s');
+
+        // @TODO: use LOGGER
+        Log::channel('queryUrl')->info($url);
     }
 
     public function handle()
     {
+        $url = $this->argument('url');
+
+
+        while (empty($url)) {
+            $url = $this->ask('Enter URL to query');
+        }
+
+        // @TODO: validate URL
+
         // Find a proxy.
-
-        // @FIXME: rewrite as ProxyService and accept dependencies via constructor (maybe even service provider)
-        $curl = curl_init('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=10000&country=all&ssl=yes&anonymity=all');
-
-        curl_setopt($curl, CURLOPT_HEADER, 0);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-
-        $proxies = explode("\n", $response);
+        $proxyList = $this->getProxyList();
 
         // Make request.
 
-        // @FIXME: validate url argument - use query service to do this so validation will be consistent if this is
-        // reused on an HTTP route.
-        $url = $this->argument('url');
-
-        // @TODO: QueryService
         // TODO: decide whether to bail immediately on failure, define number of proxies to retry with, etc.
-        foreach ($proxies as $proxy) {
-            $curl = curl_init($url);
+        $headers = $this->urlQueryService->query($url, $proxyList[0]);
 
-            curl_setopt($curl, CURLOPT_HEADER, 1);
-            curl_setopt($curl, CURLOPT_PROXY, $proxy);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-            $response = curl_exec($curl);
-
-            curl_close($curl);
-
-            if (!$response) {
-                continue;
-            }
-        }
-
-        // Output HTTP headers.
-        // @TODO: should be able to use a PSR-4(?) request obj for this
-        $parts = explode("\r\n\r\n", $response, 2);
-
-        $header = $parts[0];
-
-        $this->line($header);
+        $this->writeHeaders($headers);
 
         // Log this request.
-
-        $now = date('d/m/Y H:i:s');
-
-        // @TODO: use LOGGER
-        file_put_contents(storage_path() . '/logs/results.log', "{$now}: {$url}\r\n", FILE_APPEND);
+        $this->logRequest($url);
 
         // @TODO: use constants for exit codes
-        return 0;
+        return self::SUCCESS;
     }
 }
